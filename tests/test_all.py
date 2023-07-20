1237a5edec96e91042ac8ff922c6bb670b7704ea
heinwol <koreshkovhw@yandex.ru>
2023-07-20 18:33:43 +0300

    basic testing is complete

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,5 @@
 from itertools import starmap
+from typing import Generator
 import pytest
 
 from sponge_networks.resource_networks import *
@@ -8,7 +9,18 @@ from sponge_networks.utils.utils import *
 
 DescriptorPair = TypedDict(
     "DescriptorPair",
-    {"node_descriptor": dict[Node, int], "idx_descriptor": TypedMapping[int, Node]},
+    {
+        "node_descriptor": dict[Node, int],
+        "idx_descriptor": TypedMapping[int, Node],
+    },
+)
+
+ProtocolPair = TypedDict(
+    "ProtocolPair",
+    {
+        "result_protocol": pd.DataFrame,
+        "standard_protocol": pd.DataFrame,
+    },
 )
 
 
@@ -18,7 +30,7 @@ def resourceDiGraph_from_array(arr: npt.ArrayLike) -> ResourceDiGraph:
     return rn
 
 
-rns_with_simulations = [
+rns_with_simulations_raw = [
     {
         # если заменить 2 на 1 в первой строке, то аттрактором станет другая вершина!
         "rn": resourceDiGraph_from_array(
@@ -36,26 +48,94 @@ rns_with_simulations = [
                 "correct_file": "g1.xlsx",
             },
         ],
-    }
+    },
+    {
+        "rn": resourceDiGraph_from_array(
+            [
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [3, 1, 1, 1, 1],
+                [4, 1, 1, 1, 1],
+                [5, 1, 1, 1, 1],
+            ]
+        ),
+        "simulations": [
+            {
+                "initial_state": [0, 40, 0, 0, 0],
+                "n_iters": 40,
+                "correct_file": "g2.xlsx",
+            },
+        ],
+    },
 ]
 
-# @pytest.fixture
-# def interesting_graph():
-#     ig = np.array(
-#         [
-#             [0, 0, 2, 0],  # если заменить 2 на 1, то аттрактором станет другая вершина!
-#             [0, 0, 0, 4],
-#             [3, 2, 0, 0],
-#             [1, 5, 0, 0],
-#         ]
-#     )
-#     interesting_graph = ResourceDiGraph(
-#         nx.from_numpy_array(ig, create_using=nx.DiGraph)
-#     )
-#     sim = interesting_graph.run_simulation([20, 10, 6, 12], 200)
-#     plot_simulation(interesting_graph, sim)
+
+class _TestsProvider:
+    def __init__(self, rns_with_simulations_raw) -> None:
+        res = []
+        for d in rns_with_simulations_raw:
+            elt = {}
+            elt["rn"] = d["rn"]
+            elt["simulations"] = [
+                {
+                    "result_sim": d["rn"].run_simulation(
+                        initial_state=it["initial_state"],
+                        n_iters=it["n_iters"],
+                    ),
+                    "standard_protocol": pd.read_excel(
+                        f"tests/data/{it['correct_file']}"
+                    ).set_index("t", drop=True),
+                }
+                for it in d["simulations"]
+            ]
+            res.append(elt)
+        self.rns_with_simulations = res
+
+    def all_simulations(self) -> Generator[StateArray, None, None]:
+        for d in self.rns_with_simulations:
+            for sims in d["simulations"]:
+                yield sims["result_sim"]
+
+    def all_descriptor_pairs(self) -> Generator[DescriptorPair, None, None]:
+        for d in self.rns_with_simulations:
+            yield {
+                "idx_descriptor": d["rn"].idx_descriptor,
+                "node_descriptor": d["rn"].node_descriptor,
+            }
+
+    def all_protocol_pairs(
+        self,
+    ) -> Generator[ProtocolPair, None, None]:
+        for d in self.rns_with_simulations:
+            for sims in d["simulations"]:
+                yield {
+                    "result_protocol": sims["result_sim"].simple_protocol(),
+                    "standard_protocol": sims["standard_protocol"],
+                }
 
 
+tests_provider = _TestsProvider(rns_with_simulations_raw)
+
+
+@pytest.fixture(params=tests_provider.all_simulations())
+def simulation(request):
+    return request.param
+
+
+@pytest.fixture(params=tests_provider.all_descriptor_pairs())
+def desc_pair(request):
+    return request.param
+
+
+@pytest.fixture(params=tests_provider.all_protocol_pairs())
+def protocol_pair(request):
+    return request.param
+
+
+# @pytest.mark.parametrize(
+#     "simulation",
+#     [(1, 2), (3, 4)],
+# )
 def test_resource_is_preserved(simulation: StateArray):
     sim_sum = simulation.states_arr.sum(axis=1)
     assert np.allclose(sim_sum[0], sim_sum, atol=1e-7)
@@ -72,11 +152,9 @@ def test_node_descriptor_is_trivial(desc_pair: DescriptorPair):
     assert all(starmap(lambda k, v: k == v, desc_pair["node_descriptor"].items()))
 
 
-def test_lala():
-    print("lala")
-    assert 1 == 1
-
-
-def test_bebe():
-    print("bebe")
-    assert 2 == 1
+def test_protocols_are_equal(protocol_pair: ProtocolPair):
+    # print(protocol_pair["standard_protocol"].columns)
+    assert np.allclose(
+        protocol_pair["result_protocol"],
+        protocol_pair["standard_protocol"],
+    )
