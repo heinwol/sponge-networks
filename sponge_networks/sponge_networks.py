790ab760b396139bb1e5cd5f469a4cf0c15e8ad6
heinwol <heinvvol@gmail.com>
2023-08-07 17:28:28 +0300

    added grid tyoe comments

--- sponge_networks/sponge_networks.py
+++ sponge_networks/sponge_networks.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from .utils.utils import *
 from .resource_networks import *
 
@@ -13,3 +14,53 @@ class ResourceNetworkGreedy(ResourceNetwork):
         return np.minimum(  # type: ignore
             q_rest * self.stochastic_matrix, self.adjacency_matrix
         ) + np.diag(q_contained)
+
+
+GridType: TypeAlias = Literal["triangular", "hexagonal", "grid_2d"]
+
+# grid types are as follows:
+# =========================
+# (visually inaccurate, but are isomorphic and orientation is preserved)
+#
+#    hexagonal:
+# ┌────────┐     ┌────────┐     ┌────────┐     ┌────────┐
+# │ (0, 1) │ ─── │ (0, 2) │ ─── │ (1, 2) │ ─── │ (1, 1) │
+# └────────┘     └────────┘     └────────┘     └────────┘
+#   │                                            │
+#   │                                            │
+#   │                                            │
+# ┌────────┐     ┌────────┐                      │
+# │ (0, 0) │ ─── │ (1, 0) │──────────────────────┘
+# └────────┘     └────────┘
+#
+#    triangular:
+#           ┌────────┐
+#   ┌───────│ (0, 1) │────┐
+#   │       └────────┘    │
+# ┌────────┐          ┌────────┐
+# │ (0, 0) │ ──────── │ (1, 0) │
+# └────────┘          └────────┘
+#
+#
+#    grid_2d:
+#
+# ┌────────┐     ┌────────┐
+# │ (0, 1) │ ─── │ (1, 1) │
+# └────────┘     └────────┘
+#     │              │
+#     │              │
+# ┌────────┐     ┌────────┐
+# │ (0, 0) │ ─── │ (1, 0) │
+# └────────┘     └────────┘
+
+
+# def f(t: GridType) -> int:
+#     match t:
+#         case "grid_2d":
+#             return 1
+#         case "hexagonal":
+#             return 2
+#         case "triangular":
+#             return 3
+#         case _:
+#             raise ValueError("wrong type of grid")
